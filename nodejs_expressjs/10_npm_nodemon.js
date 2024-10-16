6482ad69a5ef440a378262b87f3cbb3d454c0b42
luricisme <trinhnguyenluong182306@gmail.com>
2024-10-16 10:15:39 +0700

    nodemon package

---
+++ nodejs_expressjs/10_npm_nodemon.js
@@ -0,0 +1,11 @@
+/*
+Công dung:
+- Dùng để load lại liên tục khi có sự thay đổi code --> Đỡ tốn thời gian.
+- Cú pháp: npm i nodemon -D
+- Sau đó vào file package.json: 
+ + Vào mục Script: Thêm VD: 
+   "scripts": {
+    "start": "node 9_npm_intro.js",
+    "dev": "nodemon 9_npm_intro.js"
+  },
+*/
